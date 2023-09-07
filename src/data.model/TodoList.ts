5907b972f3f6417ca74e198a3ccc192b7c3127d9
fausio luis <luisfausio@gmail.com>
2023-09-07 08:22:19 +0200

    TodoList

---
+++ src/data.model/TodoList.ts
@@ -0,0 +1,17 @@
+import { TodoItem } from "./TodoItem";
+export class TodoList {
+
+  constructor(public user: string, private todoItems: TodoItem[] = []) {
+
+  }
+
+
+  get items(): readonly TodoItem[] {
+    return this.todoItems;
+  }
+
+  AddItem(task: string) {
+    this.todoItems.push(new TodoItem(task));
+  }
+
+}
